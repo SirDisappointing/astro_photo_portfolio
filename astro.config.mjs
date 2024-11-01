ae1ded07cbf2114bbc2704c8c9134deb691a8906
daniel-de-wit <d.dewit@speakup.nl>
2024-11-01 15:46:28 +0100

    project init

---
+++ astro.config.mjs
@@ -0,0 +1,8 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+import vercel from '@astrojs/vercel/serverless';
+
+export default defineConfig({
+  output: 'server',
+  adapter: vercel(),
+});
